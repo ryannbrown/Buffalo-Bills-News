2c21f84a36cd4426bcf8489b5158fdcbbb54dcb3
ryannbrown <44414542+ryannbrown@users.noreply.github.com>
2019-06-23 19:45:19 -0400

    opening links in new window

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ $.getJSON("/articles", function(data) {
     // For each one
     for (var i = 0; i < data.length; i++) {
       // Display the apropos information on the page
-      $("#articles").append("<div class='card'><div class='card-body'><h1 data-id='" + data[i]._id + "'>" + data[i].title + "</h1>" + "<br />" + "<p>" + data[i].summary + "</p>"  + "<br>" + "<a>"  + data[i].link + "</a></div></div>");
+      $("#articles").append("<div class='card'><div class='card-body'><h1 data-id='" + data[i]._id + "'>" + data[i].title + "</h1>" + "<br />" + "<p>" + data[i].summary + "</p>"  + "<br>" + "<a href='" + data[i].link + "'target='_blank''>"  + data[i].link + "</a></div></div>");
     }
   });
   
